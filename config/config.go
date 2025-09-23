d08afbcf502ad6faa3dceb7558170240d4cc979b
ut002965 <lihaipeng@uniontech.com>
2025-09-23 20:02:20 +0800

    yaml配置文件自定义

--- config/config.go
+++ config/config.go
@@ -11,7 +11,17 @@ import (
 var (
         ScrapeUrl       *string
         Insecure        *bool
+        DefaultSettings = Settings{
+                //ScrapeUri: "http://127.0.0.1:24220/api/plugins.json",
+                Insecure: false,
+        }
+
 )
+
+type Settings struct {
+        ScrapeUri string `yaml:"scrape_uri"`
+        Insecure  bool   `yaml:"insecure"`
+}
 
 func init() {
         ScrapeUrl = kingpin.Flag("scrape_uri","Scrape URI").Short('s').String()
