3cf979522943800f869167cf67b45e9ca185f613
ut002965 <lihaipeng@uniontech.com>
2025-09-23 19:59:08 +0800

    增加HTTP/HTTPS链接有效性验证

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@
 package config
 
 import (
+	"uos-dovecot-exporter/pkg/utils"
         "github.com/alecthomas/kingpin"
         "github.com/sirupsen/logrus"
 )
@@ -16,5 +17,18 @@ func init() {
         ScrapeUrl = kingpin.Flag("scrape_uri","Scrape URI").Short('s').String()
 
         Insecure = kingpin.Flag("insecure","Ignore server certificate if using https, Default: false.").Bool()
+
+        if *ScrapeUrl != "" {
+                if err := utils.ValidateURI(*ScrapeUrl); err != nil {
+                        logrus.Warnf("Invalid scrape uri: %s", err)
+                        logrus.Warnf("Use default scrape uri: %s", DefaultSettings.ScrapeUri)
+                        *ScrapeUrl = DefaultSettings.ScrapeUri
+                }
+        }
+
+        if *Insecure {
+                logrus.Warn("Insecure mode enabled, this is not recommended for production use.")
+        }
+
 }
 
