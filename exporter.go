abd14b497e53ddd30794f86ff2972f6dc2133f9a
ut002965 <lihaipeng@uniontech.com>
2025-09-24 16:52:19 +0800

    完善导出器异常功能

--- exporter.go
+++ exporter.go
@@ -11,15 +11,20 @@ import (
 func Run(name string, version string) error {
         logger.InitDefaultLog()
         s := server.NewServer(name, version)
+	s.PrintVersion()
         go func() {
                 err := s.Run()
                 if err != nil {
                         logrus.Errorf("Run error: %v", err)
+			s.Error = err
                 }
                 s.Exit()
         }()
-
-        logrus.Info("init Run ....")
-        return nil
+        select {
+        case <-s.ExitSignal:
+                s.Stop()
+                logrus.Info("Exit exporter server completed")
+                return s.Error
+        }
 }
 
